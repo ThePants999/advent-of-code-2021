9e14e7e32093e4c0943d3f48b272e880c68bcda0
Chris Paterson <chris.paterson@metaswitch.com>
2021-12-01 19:12:39 +0000

    And so it begins

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,8 @@
+use itertools::Itertools;
+
 pub fn day1(input_lines: &[String]) -> (u64, u64) {
-    let expenses: Vec<u64> = input_lines.iter().map(|line| line.parse::<u64>().expect("Failed to parse input")).collect();
-    let part1 = 0u64;
-    let part2 = 0u64;
+    let depths: Vec<u64> = input_lines.iter().map(|line| line.parse::<u64>().expect("Failed to parse input")).collect();
+    let part1 = depths.iter().tuple_windows().filter(|(first, second)| second > first).count() as u64;
+    let part2 = depths.iter().tuple_windows::<(_, _, _)>().map(|(first, second, third)| first + second + third).tuple_windows().filter(|(first, second)| second > first).count() as u64;
     (part1, part2) 
 }
