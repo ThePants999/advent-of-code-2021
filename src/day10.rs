8ce722f00640d1b0f938ec5e4b04e6916033e77c
Chris Paterson <chris.paterson@metaswitch.com>
2021-12-10 10:27:24 +0000

    Day 10, yawn

---
+++ src/day10.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+pub fn day10(input_lines: &[String]) -> (u64, u64) {
+    let illegal_scores: HashMap<char, u64> = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+
+    let mut illegal_score = 0u64;
+    let mut incomplete_scores: Vec<u64> = Vec::with_capacity(input_lines.len());
+    for line in input_lines {
+        let mut chunks: Vec<char> = Vec::with_capacity(line.len() / 2);
+        let mut legal = true;
+        for c in line.chars() {
+            let current_chunk = chunks.last();
+            match c {
+                '(' => chunks.push(')'),
+                '[' => chunks.push(']'),
+                '{' => chunks.push('}'),
+                '<' => chunks.push('>'),
+                ')' | ']' | '}' | '>' => {
+                    if current_chunk == Some(&c) {
+                        chunks.pop();
+                    } else {
+                        illegal_score += illegal_scores.get(&c).unwrap();
+                        legal = false;
+                        break;
+                    }
+                },
+                _ => unreachable!("Invalid character in input"),
+            }
+        }
+
+        if legal && !chunks.is_empty() {
+            let mut incomplete_score = 0u64;
+            while !chunks.is_empty() {
+                incomplete_score *= 5;
+                incomplete_score += match chunks.pop() {
+                    Some(')') => 1,
+                    Some(']') => 2,
+                    Some('}') => 3,
+                    Some('>') => 4,
+                    _ => unreachable!(),
+                };
+            }
+            incomplete_scores.push(incomplete_score);
+        }
+    }
+    let part1 = illegal_score;
+    incomplete_scores.sort_unstable();
+    let part2 = incomplete_scores[incomplete_scores.len() / 2];
+
+    (part1,part2)
+}
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
-// mod day10;
+mod day10;
 // mod day11;
 // mod day12;
 // mod day13;
@@ -31,7 +31,7 @@ mod day9;
 use std::env;
 
 type DayFunction = fn(&[String]) -> (u64, u64);
-static DAY_FUNCTIONS: [DayFunction; 9] = [
+static DAY_FUNCTIONS: [DayFunction; 10] = [
     day1::day1,
     day2::day2,
     day3::day3,
@@ -41,7 +41,7 @@ static DAY_FUNCTIONS: [DayFunction; 9] = [
     day7::day7,
     day8::day8,
     day9::day9,
-    // day10::day10,
+    day10::day10,
     // day11::day11,
     // day12::day12,
     // day13::day13,
