1ca463fb179b1f2805a1f9996b45fc003235e7f4
Chris Paterson <chris.paterson@metaswitch.com>
2021-12-12 18:32:22 +0000

    Slightly faster day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,43 +1,54 @@
-use std::{collections::{HashSet, HashMap}, rc::{Rc, Weak}, cell::RefCell, hash::{Hash, Hasher}};
+use std::{collections::{HashSet, HashMap}, hash::Hash};
 
 pub fn day12(input_lines: &[String]) -> (u64, u64) {
-    let caves = parse_input(input_lines);
-    let start = caves.get("start").unwrap();
-    let part1 = explore(start, false).len() as u64;
-    let part2 = explore(start, true).len() as u64;
+    let (caves, cave_dict) = parse_input(input_lines);
+    let start = *cave_dict.get("start").unwrap();
+    let part1 = explore(&caves, start, false).len() as u64;
+    let part2 = explore(&caves, start, true).len() as u64;
     (part1,part2)
 }
 
-fn parse_input<'a>(input_lines: &'a [String]) -> HashMap<&'a str, Rc<RefCell<Cave>>> {
-    let mut caves: HashMap<&'a str, Rc<RefCell<Cave>>> = HashMap::with_capacity(input_lines.len());
+fn parse_input<'a>(input_lines: &'a [String]) -> (Vec<Cave<'a>>, HashMap<&'a str, usize>) {
+    let mut cave_id = 0usize;
+    let mut cave_dict: HashMap<&'a str, usize> = HashMap::with_capacity(input_lines.len());
+    let mut caves: Vec<Cave> = Vec::with_capacity(input_lines.len());
     for line in input_lines {
         let connected_caves = line.split('-').collect::<Vec<_>>();
-        let cave1 = Rc::downgrade(caves.entry(connected_caves[0]).or_insert_with(|| Rc::new(RefCell::new(Cave::new(connected_caves[0])))));
-        let cave2 = caves.entry(connected_caves[1]).or_insert_with(|| Rc::new(RefCell::new(Cave::new(connected_caves[1]))));
+        let cave1_id = *cave_dict.entry(connected_caves[0]).or_insert(cave_id);
+        if cave1_id == cave_id {
+            let cave = Cave::new(connected_caves[0], cave_id);
+            caves.push(cave);
+            cave_id += 1;
+        }
+        let cave2_id = *cave_dict.entry(connected_caves[1]).or_insert(cave_id);
+        if cave2_id == cave_id {
+            let cave = Cave::new(connected_caves[1], cave_id);
+            caves.push(cave);
+            cave_id += 1;
+        }
         if connected_caves[1] != "start" {
-            cave1.upgrade().unwrap().borrow_mut().connections.push(Rc::downgrade(cave2));
+            caves[cave1_id].connections.push(cave2_id);
         }
         if connected_caves[0] != "start" {
-            cave2.borrow_mut().connections.push(cave1);
+            caves[cave2_id].connections.push(cave1_id);
         }
     }
-    caves
+    (caves, cave_dict)
 }
 
-fn explore<'a>(start_cave: &Rc<RefCell<Cave<'a>>>, allow_duplicate_small_cave: bool) -> Vec<Path<'a>> {
-    let mut paths: Vec<Path<'a>> = Vec::new();
-    let mut paths_in_progress: Vec<Path<'a>> = Vec::new();
-    let mut paths_tried: HashSet<Path<'a>> = HashSet::new();
+fn explore(caves: &[Cave], start_cave_index: usize, allow_duplicate_small_cave: bool) -> Vec<Path> {
+    let mut paths: Vec<Path> = Vec::new();
+    let mut paths_in_progress: Vec<Path> = Vec::new();
+    let mut paths_tried: HashSet<Path> = HashSet::new();
 
-    paths_in_progress.push(Path::new(&Rc::downgrade(start_cave)));
+    paths_in_progress.push(Path::new(&caves[start_cave_index]));
     while let Some(path) = paths_in_progress.pop() {
-        let cave_rc = path.path.last().unwrap().get_cave();
-        let cave = cave_rc.borrow();
-        if cave.id == "end" {
+        let cave = &caves[*path.path.last().unwrap()];
+        if cave.name == "end" {
             paths.push(path);
         } else {
             for connection in cave.connections.iter() {
-                if let Some(new_path) = Path::add(&path, connection, allow_duplicate_small_cave) {
+                if let Some(new_path) = Path::add(&path, &caves[*connection], allow_duplicate_small_cave) {
                     if !paths_tried.contains(&new_path) {
                         paths_tried.insert(new_path.clone());
                         paths_in_progress.push(new_path);
@@ -50,15 +61,17 @@ fn explore<'a>(start_cave: &Rc<RefCell<Cave<'a>>>, allow_duplicate_small_cave: b
 }
 
 struct Cave<'a> {
-    id: &'a str,
+    name: &'a str,
+    id: usize,
     small: bool,
-    connections: Vec<Weak<RefCell<Cave<'a>>>>,
+    connections: Vec<usize>,
 }
 
 impl<'a> Cave<'a> {
-    fn new(id: &'a str) -> Self {
-        let small = id.chars().next().unwrap().is_lowercase();
+    fn new(name: &'a str, id: usize) -> Self {
+        let small = name.chars().next().unwrap().is_lowercase();
         Self {
+            name,
             id,
             small,
             connections: Vec::new(),
@@ -66,123 +79,41 @@ impl<'a> Cave<'a> {
     }
 }
 
-impl<'a> PartialEq for Cave<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-
-impl<'a> Eq for Cave<'a> {}
-
-impl<'a> Hash for Cave<'a> {
-    fn hash<H>(&self, state: &mut H)
-        where H: Hasher
-    {
-        self.id.hash(state);
-    }
-}
-
-struct CaveRef<'a>(Weak<RefCell<Cave<'a>>>);
-
-impl<'a> CaveRef<'a> {
-    fn from(cave: &Weak<RefCell<Cave<'a>>>) -> Self {
-        Self {
-            0: Weak::clone(cave)
-        }
-    }
-
-    fn get_cave(&self) -> Rc<RefCell<Cave<'a>>> {
-        self.0.upgrade().unwrap()
-    }
-}
-
-impl<'a> PartialEq for CaveRef<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.ptr_eq(&other.0)
-    }
-}
-
-impl<'a> Eq for CaveRef<'a> {}
-
-impl<'a> Clone for CaveRef<'a> {
-    fn clone(&self) -> Self {
-        Self {
-            0: Weak::clone(&self.0)
-        }
-    }
-}
-
-impl<'a> Hash for CaveRef<'a> {
-    fn hash<H>(&self, state: &mut H)
-        where H: Hasher
-    {
-        self.0.upgrade().unwrap().borrow().id.hash(state);
-    }
-}
-
-#[derive(Clone)]
-struct Path<'a> {
-    path: Vec<CaveRef<'a>>,
-    small_caves: HashSet<CaveRef<'a>>,
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct Path {
+    path: Vec<usize>,
+    small_caves: usize,
     duplicate_small_cave: bool,
 }
 
-impl<'a> Path<'a> {
-    fn new(first_cave: &Weak<RefCell<Cave<'a>>>) -> Self {
+impl Path {
+    fn new(first_cave: &Cave) -> Self {
         let path = Self {
             path: Vec::new(),
-            small_caves: HashSet::new(),
+            small_caves: 0,
             duplicate_small_cave: false,
         };
         Self::add(&path, first_cave, false).unwrap()
     }
 
-    fn add(previous: &Self, next_step: &Weak<RefCell<Cave<'a>>>, allow_duplicate_small_cave: bool) -> Option<Self> {
-        let caveref = CaveRef::from(next_step);
-        let cave_is_small = caveref.get_cave().borrow().small;
-        let visited_small_cave_before = cave_is_small && previous.small_caves.contains(&caveref);
+    fn add(previous: &Self, next_step: &Cave, allow_duplicate_small_cave: bool) -> Option<Self> {
+        let cave_is_small = next_step.small;
+        let visited_small_cave_before = cave_is_small && (previous.small_caves & (1 << next_step.id) != 0);
         if visited_small_cave_before && (!allow_duplicate_small_cave || previous.duplicate_small_cave) {
             // This path has already visited this small cave, and either we're not
             // allowed to visit one twice, or we've already visited one twice.
             None
         } else {
             let mut new_path = previous.clone();
-            new_path.path.push(caveref.clone());
+            new_path.path.push(next_step.id);
             if cave_is_small {
                 if visited_small_cave_before {
                     new_path.duplicate_small_cave = true;
                 } else {
-                    new_path.small_caves.insert(caveref);
+                    new_path.small_caves |= 1 << next_step.id;
                 }
             }
             Some(new_path)
         }
-    }
-}
-
-impl<'a> PartialEq for Path<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        let mut eq = true;
-        if self.path.len() == other.path.len() {
-            for index in 0..self.path.len() {
-                if !self.path[index].0.ptr_eq(&other.path[index].0) {
-                    eq = false;
-                    break;
-                }
-            }
-        } else {
-            eq = false;
-        }
-        eq
-    }
-}
-
-impl<'a> Eq for Path<'a> {}
-
-impl<'a> Hash for Path<'a> {
-    fn hash<H>(&self, state: &mut H)
-        where H: Hasher
-    {
-        self.path.hash(state);
     }
 }
