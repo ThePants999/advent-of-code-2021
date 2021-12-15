34543c788e17c27f83eaf91215b6a257e12305af
Chris Paterson <chris.paterson@metaswitch.com>
2021-12-15 14:05:21 +0000

    Day 15 - actually improve perf without breaking the correctness!

--- src/day15.rs
+++ src/day15.rs
@@ -1,8 +1,16 @@
-use std::collections::{HashMap, HashSet, BinaryHeap};
+use std::collections::BinaryHeap;
 
 pub fn day15(input_lines: &[String]) -> (u64, u64) {
     let mut cavern = parse_input(input_lines);
     let mut extended_cavern = cavern.extend();
+
+    for row in extended_cavern.map.iter() {
+        for node in row.iter() {
+            print!("{}", node.cost);
+        }
+        println!();
+    }
+
     let part1 = cavern.distance_top_left_to_bottom_right();
     let part2 = extended_cavern.distance_top_left_to_bottom_right();
 
@@ -10,15 +18,16 @@ pub fn day15(input_lines: &[String]) -> (u64, u64) {
 }
 
 fn parse_input(input_lines: &[String]) -> Cavern {
-    let rows = input_lines.len() as isize;
-    let cols = input_lines[0].len() as isize;
-    let nodes = (rows * cols) as usize;
-    let mut map: HashMap<(isize, isize), Node> = HashMap::with_capacity(nodes);
+    let rows = input_lines.len();
+    let cols = input_lines[0].len();
+    let mut map: Vec<Vec<Node>> = Vec::with_capacity(rows);
 
-    for (row, line) in input_lines.iter().enumerate() {
-        for (col, cost) in line.chars().map(|c| c.to_digit(10).expect("Non-numeric input") as u64).enumerate() {
-            map.insert((row as isize, col as isize), Node { cost, distance: u64::MAX });
+    for line in input_lines.iter() {
+        let mut row: Vec<Node> = Vec::with_capacity(cols);
+        for cost in line.chars().map(|c| c.to_digit(10).expect("Non-numeric input") as u64) {
+            row.push(Node { cost, distance: u64::MAX, visited: false });
         }
+        map.push(row);
     }
 
     Cavern::new(map, rows, cols)
@@ -27,13 +36,14 @@ fn parse_input(input_lines: &[String]) -> Cavern {
 struct Node {
     cost: u64,
     distance: u64,
+    visited: bool,
 }
 
 #[derive(PartialEq, Eq)]
 struct NodeDistance {
     distance: u64,
-    row: isize,
-    col: isize,
+    row: usize,
+    col: usize,
 }
 
 impl Ord for NodeDistance {
@@ -53,67 +63,76 @@ impl PartialOrd for NodeDistance {
 }
 
 struct Cavern {
-    map: HashMap<(isize, isize), Node>,
-    visited: HashSet<(isize, isize)>,
+    map: Vec<Vec<Node>>,
     heap: BinaryHeap<NodeDistance>,
-    rows: isize,
-    cols: isize,
+    rows: usize,
+    cols: usize,
 }
 
 impl Cavern {
-    fn new(map: HashMap<(isize, isize), Node>, rows: isize, cols: isize) -> Self {
-        let nodes = (rows * cols) as usize;
-        let mut cavern = Self { map, visited: HashSet::with_capacity(nodes), heap: BinaryHeap::with_capacity(nodes), rows, cols };
-        let mut start = cavern.map.get_mut(&(0, 0)).unwrap();
-        cavern.heap.push(NodeDistance { distance: 0, row: 0, col: 0 });
+    fn new(map: Vec<Vec<Node>>, rows: usize, cols: usize) -> Self {
+        let mut cavern = Self { map, heap: BinaryHeap::with_capacity(rows * cols), rows, cols };
+        let mut start = &mut cavern.map[0][0];
         start.distance = 0;
+        cavern.heap.push(NodeDistance { distance: 0, row: 0, col: 0 });
         cavern
     }
 
     fn extend(&self) -> Self {
-        let nodes = (self.rows * self.cols * 25) as usize;
-        let mut map: HashMap<(isize, isize), Node> = HashMap::with_capacity(nodes);
-        for ((original_row, original_col), node) in self.map.iter() {
-            for map_row in 0..5 {
+        let mut map: Vec<Vec<Node>> = Vec::with_capacity(self.rows * 5);
+        for map_row in 0..5 {
+            for original_row in 0..self.rows {
+                let mut row = Vec::with_capacity(self.cols * 5);
                 for map_col in 0..5 {
-                    let row = original_row + (self.rows * map_row);
-                    let col = original_col + (self.cols * map_col);
-                    let mut cost = node.cost + map_row as u64 + map_col as u64;
-                    if cost > 9 { cost %= 9; }
-                    map.insert((row, col), Node { cost, distance: u64::MAX });
+                    for original_col in 0..self.cols {
+                        let node = &self.map[original_row][original_col];
+                        let mut cost = node.cost + map_row as u64 + map_col as u64;
+                        if cost > 9 { cost %= 9; }
+                        row.push(Node { cost, distance: u64::MAX, visited: false });
+                    }
                 }
+                map.push(row);
             }
         }
         Self::new(map, self.rows * 5, self.cols * 5)
     }
 
     fn distance_top_left_to_bottom_right(&mut self) -> u64 {
-        let final_coords = (self.rows - 1, self.cols - 1);
+        let final_row = self.rows - 1;
+        let final_col = self.cols - 1;
 
-        while !self.visited.contains(&final_coords) {
+        while !self.map[final_row][final_col].visited {
             let next = self.heap.pop().unwrap();
-            if !self.visited.contains(&(next.row, next.col)) {
+            if !self.map[next.row][next.col].visited {
                 self.visit(next.row, next.col);
             }
         }
 
-        self.map.get(&final_coords).unwrap().distance
+        self.map[final_row][final_col].distance
     }
 
-    fn visit(&mut self, row: isize, col: isize) {
-        let distance = self.map.get(&(row, col)).unwrap().distance;
+    fn visit(&mut self, row: usize, col: usize) {
+        let distance = self.map[row][col].distance;
 
-        self.consider(row - 1, col, distance);
-        self.consider(row + 1, col, distance);
-        self.consider(row, col - 1, distance);
-        self.consider(row, col + 1, distance);
+        if row > 0 {
+            self.consider(row - 1, col, distance);
+        }
+        if row < self.rows - 1 {
+            self.consider(row + 1, col, distance);
+        }
+        if col > 0 {
+            self.consider(row, col - 1, distance);
+        }
+        if col < self.cols - 1 {
+            self.consider(row, col + 1, distance);
+        }
 
-        self.visited.insert((row, col));
+        self.map[row][col].visited = true;
     }
 
-    fn consider(&mut self, row: isize, col: isize, from_distance: u64) {
-        if row >= 0 && row < self.rows && col >= 0 && col < self.rows && !self.visited.contains(&(row, col)) {
-            let mut node = self.map.get_mut(&(row, col)).unwrap();
+    fn consider(&mut self, row: usize, col: usize, from_distance: u64) {
+        if !self.map[row][col].visited {
+            let mut node = &mut self.map[row][col];
             let new_distance = from_distance + node.cost;
             if new_distance < node.distance {
                 node.distance = new_distance;
